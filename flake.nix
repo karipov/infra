58b4e1d53b379a8abc5beb98a12d871ab1fe1491
Komron Aripov <karipov@protonmail.com>
2025-11-22 17:39:06 -0500

    feat: nixos

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Komron's NixOS homelab infra";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+        ];
+      };
+    };
+}
