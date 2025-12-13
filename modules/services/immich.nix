7fb7b90f833b0add498ade2e91efd7acf1e1eb14
Komron Aripov <karipov@protonmail.com>
2025-12-12 23:36:00 -0500

    fix: unwrap immich out of config

--- modules/services/immich.nix
+++ modules/services/immich.nix
@@ -1,22 +1,20 @@
 { config, ... }:
 
 {
-  config = {
-    services.immich = {
-      enable = true;
-    };
+  services.immich = {
+    enable = true;
+  };
 
-    # open firewall port for Immich web UI and API
-    # default port is 2283
-    networking.firewall.allowedTCPPorts = [ 2283 ];
+  # open firewall port for Immich web UI and API
+  # default port is 2283
+  networking.firewall.allowedTCPPorts = [ 2283 ];
 
-    # configure immich user and add to video and render groups for GPU access (hardware acceleration)
-    users.groups.immich = {};
-    users.users.immich = {
-      isSystemUser = true;
-      group = "immich";
-      extraGroups = [ "video" "render" ];
-    };
+  # configure immich user and add to video and render groups for GPU access (hardware acceleration)
+  users.groups.immich = {};
+  users.users.immich = {
+    isSystemUser = true;
+    group = "immich";
+    extraGroups = [ "video" "render" ];
   };
 }
 
