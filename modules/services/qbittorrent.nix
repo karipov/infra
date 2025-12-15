91ff7d374153c0f75fc8ba4cc618c45ba86dfd13
Komron Aripov <karipov@protonmail.com>
2025-12-15 01:37:01 -0500

    fix: hopefully media group should have full access to qbittorrent files now

--- modules/services/qbittorrent.nix
+++ modules/services/qbittorrent.nix
@@ -7,5 +7,8 @@
 
   # add qbittorrent user to media group for access to downloads/media directories
   users.users.qbittorrent.extraGroups = [ "media" ];
+
+  # set umask so qbittorrent creates files/dirs with group write (0664/2775)
+  systemd.services.qbittorrent.serviceConfig.UMask = "0002";
 }
 
