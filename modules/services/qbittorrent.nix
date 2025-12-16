9ab838456045369c28ebd925ede6665f9414708d
Komron Aripov <karipov@protonmail.com>
2025-12-15 20:48:35 -0500

    fix: open direct port qbit

--- modules/services/qbittorrent.nix
+++ modules/services/qbittorrent.nix
@@ -10,5 +10,8 @@
 
   # set umask so qbittorrent creates files/dirs with group write (0664/2775)
   systemd.services.qbittorrent.serviceConfig.UMask = "0002";
+
+  # direct peering
+  networking.firewall.allowedTCPPorts = [ 10507 ];
 }
 
