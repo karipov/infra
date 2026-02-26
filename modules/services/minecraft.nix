f4c334cab1c7d1612d8f8f3c84d25423ec698b4d
Komron Aripov <karipov@protonmail.com>
2026-02-25 22:34:01 -0500

    chroe: mc cleanup

--- modules/services/minecraft.nix
+++ modules/services/minecraft.nix
@@ -11,10 +11,10 @@
     whitelist = {
       veroshko = "1352065f-addd-4f25-bab1-3fc5cad65254";
       karipov = "5ec1441c-e7ea-467e-9076-8b73e6970059";
+      hibiscotti = "454f0dfc-2837-44a0-9343-4b36ce2c7ce7";
     };
 
     serverProperties = {
-      # MOTD: § = formatting (centering via leading spaces often stripped by client/server)
       motd = "§bturtle bay §aserver§r\\n§8(c) komron§r";
       "max-players" = 10;
       "level-seed" = "46182117";
